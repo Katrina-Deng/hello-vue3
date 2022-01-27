51ec09cfd24e314450936b6a80d281a07f260a08
dy <dengyu@my-itservice.com.cn>
2022-01-27 17:29:48 +0800

    init

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,16 +3,22 @@ module.exports = {
   env: {
     node: true
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/standard',
-    '@vue/typescript/recommended'
-  ],
+  extends: ['plugin:vue/vue3-essential', '@vue/standard', '@vue/typescript/recommended'],
   parserOptions: {
     ecmaVersion: 2020
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    indent: 'off',
+    'space-before-function-paren': [
+      'error',
+      {
+        anonymous: 'never',
+        named: 'never',
+        asyncArrow: 'never'
+      }
+    ]
   }
 }
---
+++ src/components/setup.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>123</div>
+</template>
+
+<script>
+  import { defineComponent } from 'vue'
+  import { getData } from '@/api/data'
+  export default defineComponent({
+    name: 'setup',
+    props: {
+      msg: String
+    },
+    setup() {
+      let data = []
+      const getDetailData = async () => {
+        data = await getData()
+      }
+      
+      return {
+        data,
+        getDetailData
+      }
+    }
+  })
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h3 {
+    margin: 40px 0 0;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+</style>
